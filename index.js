6093937a94900ee69fe02802ae618b25087a6327
Alexandro Cano <alexandro.cano@tecsup.edu.pe>
2025-10-10 08:43:12 -0500

    Proyecto limpio inicializado correctamente

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('¡Hola Mundo desde Node.js desplegado!');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor ejecutándose en puerto ${port}`);
+});
