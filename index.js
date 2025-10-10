9ba859f23cccc85df7a0bf735a68590d1c1fb143
Alexandro Cano <alexandro.cano@tecsup.edu.pe>
2025-10-10 09:58:55 -0500

    Agregado CV en HTML y configurado servidor estático

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta raíz (Render mostrará el index.html automáticamente)
 app.get('/', (req, res) => {
-    res.send('¡Hola Mundo desde Node.js desplegado!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Servidor ejecutándose en puerto ${port}`);
+  console.log(`Servidor ejecutándose en puerto ${port}`);
 });
